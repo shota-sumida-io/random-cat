c6f0de5df92d99b1c8ba7933c28c4f1a5d619e2c
shota-sumida-io <shota.sumida@industry-one.com>
2024-06-19 22:34:52 +0900

    init

---
+++ pages/_index.tsx
@@ -0,0 +1,38 @@
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+
+const IndexPage: NextPage = () => {
+    const [imageUrl, setImageUrl] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(true);
+    useEffect(() => {
+        fetchImage().then((newImage) => {
+            setImageUrl(newImage.url);
+            setLoading(false);
+        });
+    }, []);
+
+    const handleClick = async () => {
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url);
+        setLoading(false);
+    };
+
+    return (
+        <div>
+            <button onClick={handleClick}>他のにゃんこも見る</button>
+            <div>{loading || <img src={imageUrl} />}</div>
+        </div>
+    );
+};
+export default IndexPage;
+
+type Image = {
+    url: string;  
+};
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+};
